42e1beda46832e3627c1eab7eea697a8e062c299
Vlad Berdyshev <daryaaay@gmail.com>
2019-07-09 17:41:34 +0300

    add auth controller

---
+++ AuthToken/AuthToken/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthToken.Controllers
+{
+    [Route("api/Auth")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        [Route("LogIn")]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "Perfecto!" });
+        }
+    }
+}
